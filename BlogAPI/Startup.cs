a4c18c7e94ebb9323b0d4a858e63c746d70bc76e
GustavoTagli <taglisouza.0304@hotmail.com>
2022-08-27 14:39:37 -0300

    @GustavoTagli [Realizado o deploy]

--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -36,8 +36,18 @@ namespace BlogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configuração de Banco de Dados
-            services.AddDbContext<BlogPessoalContexto>(opt => opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            // Configuraçãp Banco de Dados
+            if (Configuration["Enviroment:Start"] == "PROD")
+            {
+                services.AddEntityFrameworkNpgsql()
+                .AddDbContext<BlogPessoalContexto>(opt =>
+                opt.UseNpgsql(Configuration["ConnectionStringsProd:DefaultConnection"]));
+            }
+            else
+            {
+                services.AddDbContext<BlogPessoalContexto>(opt =>
+                opt.UseSqlServer(Configuration["ConnectionStringsDev:DefaultConnection"]));
+            }
 
             // Configuração de Repositorios
             services.AddScoped<IUsuario, UsuarioRepositorio>();
@@ -127,6 +137,14 @@ namespace BlogAPI
             }
 
             // Ambiente de produção
+            contexto.Database.EnsureCreated();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogPessoal v1");
+                c.RoutePrefix = string.Empty;
+            });
+
             // Rotas
             app.UseRouting();
             
